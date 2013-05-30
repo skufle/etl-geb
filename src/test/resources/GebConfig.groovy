7d57e9b95ad98e98f511b4fc773c599ed86a1c82
skufle <github@munklaursen.dk>
2013-05-30 11:22:18 +0200

    Nu med Chrome på Windows og en IE driver der ikke rigtig virker.

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -6,6 +6,7 @@
 
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.ie.InternetExplorerDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 
@@ -15,20 +16,28 @@ waiting {
 
 environments {
 
-  // run via “./gradlew chromeTest”
+  // kør med “./gradlew chromeTest”
   // See: http://code.google.com/p/selenium/wiki/ChromeDriver
   chrome {
-    System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver2_mac32_0.8/chromedriver");
+    System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/win/chromedriver.exe")
+    // System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver2_mac32_0.8/chromedriver")
     driver = { new ChromeDriver() }
   }
 
-  // run via “./gradlew firefoxTest”
+  // kør med “./gradlew firefoxTest”
   // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
   firefox {
     driver = { new FirefoxDriver() }
   }
 
-  // run via “./gradlew remoteTest”
+  // kør med “./gradlew ieTest”
+  // See: http://code.google.com/p/selenium/wiki/InternetExplorerDriver
+  ie {
+    System.setProperty('webdriver.ie.driver', "src/test/resources/drivers/win/IEDriverServer.exe")
+    driver = { new InternetExplorerDriver() }
+  }
+
+  // kør med “./gradlew remoteTest”
   // See: http://code.google.com/p/selenium/wiki/RemoteWebDriver
   // Husk at sætte GEB_SAUCE_LABS_USER og GEB_SAUCE_LABS_ACCESS_PASSWORD inden afvikling
   remote {
@@ -50,7 +59,6 @@ environments {
 */
     driver = { new RemoteWebDriver(url, capabilities) }
   }
-
 }
 
-// To run the tests with all browsers just run “./gradlew test”
+// Afvikling af alle tests, kør:  “./gradlew test”
